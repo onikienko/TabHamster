beaf5873bdb6c00c2be58d9826401591e60fd379
Michael Onikienko <michael.onikienko@gmail.com>
2015-04-10 10:52:19 +0300

    reload ext on change session_watcher state
    fix locales

--- js/options.js
+++ js/options.js
@@ -1,4 +1,8 @@
 var main_tabs = new Tabs('#main_tabs');
 
-document.querySelector('#session_watcher').addEventListener('change', function () {
+document.addEventListener('optionSaved', function (event) {
+    // reload extension after toggle session watcher option
+    if (event.detail.val.hasOwnProperty('session_watcher') && event.detail.success) {
+        chrome.runtime.reload();
+    }
 }, false);
